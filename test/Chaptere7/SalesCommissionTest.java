4568a793b446fbbb52d9fc41b8ff5d0830f02b70
Kingsley Nwafor <kingsleynwafor54@gmail.com>
2021-04-28 09:49:07 +0100

    chapter

---
+++ test/Chaptere7/SalesCommissionTest.java
@@ -0,0 +1,24 @@
+package Chaptere7;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SalesCommission {
+    SalesCommission salesCommission;
+    @BeforeEach
+    void beforeEach(){
+        salesCommission=new SalesCommission();
+    }
+
+@AfterEach
+      void afterEach(){
+          salesCommission=null;
+    }
+@Test
+void methodCanCalculateSalesCommissionWithCounter(){
+        
+}
+}
+
+
